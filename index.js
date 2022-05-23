e9832b916499f0b4900ad8f96e3f75a87838c290
Oriol Pérez <oriol@fxstreet.com>
2022-05-23 09:50:38 +0200

    up

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const puppeteer = require('puppeteer');
-const domain = process.argv[2];
-const selector = process.argv[3];
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+const domain = process.argv[2] || "https://www.fxstreet.com/";
+const selector = process.argv[3] || "[fxs_name^='highlighted']";
+const attributeName = process.argv[4] || "fxs_name";
+
+const csvWriter = createCsvWriter({
+    path: `data${selector}.csv`,
+    header: [
+        { id: 'page', title: 'PAGE' },
+        { id: 'attributeValue', title: 'ATTRIBUTE VALUE' },
+    ]
+});
+
+
 const unvisitedUrls = [];
 const visitedUrls = [];
 if (!domain) {
@@ -9,17 +22,51 @@ if (!domain) {
 if (!selector) {
     throw "Please provide a selector to search as second argument";
 }
+
 async function run(url, selector) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
+
+    await page.setCookie(
+        {
+            name: "PopupAd_roadblocks",
+            value: "true",
+            url: "https://www.fxstreet.com",
+            httpOnly: false
+        },
+        {
+            name: "policyAccepted",
+            value: "",
+            url: "https://www.fxstreet.com",
+            httpOnly: false
+        }
+    );
+
     try {
-        await page.goto(url);
+        await page.goto(url, { waitUntil: 'networkidle2', timeout: 0 });
+
         const selectorExists = await page.evaluate((selector) => {
             return Promise.resolve(Array.from(document.querySelectorAll(selector)).length > 0);
         }, selector);
-        if(selectorExists){
-            console.log(url);
-        }
+
+        const attributes = await page.evaluate((selector,attributeName) => {
+            return Promise.resolve(
+                Array.from(
+                    document.querySelectorAll(selector)).map(
+                        element => element.getAttribute(attributeName)
+                    )
+            );
+        },selector,attributeName);
+
+        if (selectorExists) {
+            attributes.map(async (attributeValue) => {
+                await csvWriter.writeRecords([{
+                    page: url,
+                    attributeValue: attributeValue
+                }]);
+            })
+        };
+
         const domainUrls = await page.evaluate((domain) => {
             return Promise.resolve(Array.from(document.querySelectorAll(`[href^='${domain}']`)).map(item => item.href));
         }, domain);
@@ -32,11 +79,11 @@ async function run(url, selector) {
         console.log(error.message);
     }
     browser.close();
-    if(unvisitedUrls.length>0){
+    if (unvisitedUrls.length > 0) {
         const nextUrl = unvisitedUrls.pop();
         visitedUrls.push(nextUrl);
-        await run(nextUrl, selector)    
-    }else{
+        await run(nextUrl, selector)
+    } else {
         console.log('Exiting process');
         process.exit();
     }
